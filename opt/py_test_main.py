9eacacdd6fa17ce18019adce461903c5834f5942
sugiyama404 <mmingriha@gmail.com>
2023-03-15 13:29:06 +0900

    ok

--- opt/py_test_main.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from flask_mod import app
-
-
-def test_flask_simple():
-    app.config['TESTING'] = True
-    client = app.test_client()
-    result = client.get('/')
-    assert b'root' == result.data
